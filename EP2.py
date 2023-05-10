2cb23a27fb1df6b5542bccbb38fb6dc181fb1f3f
felipequeirox <fepodqueiroz@gmail.com>
2023-05-10 14:43:50 -0300

    Posicionando Frota

--- EP2.py
+++ EP2.py
@@ -1,7 +1,9 @@
 def define_posicoes(linha, coluna, orientacao, tamanho):
     posicoes = []
     for i in range(tamanho):
-        if orientacao == "horizontal":
+        if orientacao == 0:
+            posicoes.append([linha,coluna])
+        elif orientacao == "horizontal":
             posicao = [linha, coluna+i]
             posicoes.append(posicao)
         elif orientacao == "vertical":
@@ -47,3 +49,77 @@ def afundados (frota, tabuleiro):
             if afundado_y == len(posicoes):
                 afundado_x += 1
     return afundado_x
+
+def posicao_valida(frota, linha, coluna, orientacao, tamanho):
+    posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
+    for navios in frota.values():
+        for navio in navios:
+            for posicao2 in posicoes:
+                if posicao2 in navio:
+                    return False
+    for posicao2 in posicoes:
+        primeiro = posicao2[0] < 0
+        segundo = posicao2[0] >= 10
+        terceiro = posicao2[1] < 0
+        quarto = posicao2[1] >= 10
+        if  primeiro or segundo or  terceiro or quarto:
+            return False
+    return True 
+
+#Posicionando Frota
+
+tamanhos = {"porta-aviões":4,
+    "navio-tanque":3,
+    "contratorpedeiro":2,
+    "submarino": 1}
+
+frota = {"porta-aviões":[],
+    "navio-tanque":[],
+    "contratorpedeiro":[],
+    "submarino": []}
+
+for nome_barco in frota.keys():
+    
+    contador = 0 
+
+    if nome_barco == 'porta-aviões':
+        i = 1
+    elif nome_barco == 'navio-tanque':
+        i = 2
+    elif nome_barco == 'contratorpedeiro':
+        i = 3
+    elif nome_barco == 'submarino':
+        i = 4
+    
+    while contador < i:
+        
+        print(f'Insira as informações referentes ao navio {nome_barco} que possui tamanho {tamanhos[nome_barco]}')
+        
+        linha = int(input('Linha: '))
+        coluna = int(input('Coluna: '))
+
+        if nome_barco != 'submarino':
+            orientacao = int(input('[1] Vertical [2] Horizontal >'))
+            if orientacao == 1:
+                orientacao = 'vertical'
+            elif orientacao == 2:
+                orientacao = 'horizontal'
+        
+        if nome_barco == 'submarino':
+            orientacao = 'vertical'
+
+        if orientacao == '1':
+            orientacao = 'vertical'
+        
+        elif orientacao == '2':
+            orientacao = 'horizontal'
+
+        pos = posicao_valida (frota,linha,coluna,orientacao,tamanhos[nome_barco])
+        if pos == False:
+            print('Esta posição não está válida!')
+
+        else:
+            preenche_frota(frota,nome_barco,linha,coluna,orientacao,tamanhos[nome_barco])
+            contador += 1
+
+print(frota)
